f37608d0196eef26f1ccfdf8d87778026be8b83c
AntonioRubiaSol <117274069+AntonioRubiaSol@users.noreply.github.com>
2022-11-02 14:26:11 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String... args){
-        System.out.println("Hello");
+        System.out.println("Sup");
         System.out.println("Hello guys");
     }
 }
